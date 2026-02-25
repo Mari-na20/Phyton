7cc0d8b2750b9bc6f883c21ba0183568645951ea
Mari-na20 <mportugalsilva@icloud.com>
2026-02-25 15:25:40 -0300

    new file:   CRUD - Questionario/db.py 	new file:   CRUD - Questionario/main.py 	new file:   SQL database/Pipfile 	new file:   SQL database/Pipfile.lock 	modified:   SQL database/SQL.classes.py 	modified:   SQL database/instance/dados.db 	modified:   diary-bra-main-main/diary-bra-main-main/main.py 	renamed:    diary-bra-main-main/diary-bra-main-main/Pipfile -> diary-bra-main-main/instance/diary-bra-main-main/Pipfile 	renamed:    diary-bra-main-main/diary-bra-main-main/Pipfile.lock -> diary-bra-main-main/instance/diary-bra-main-main/Pipfile.lock 	renamed:    diary-bra-main-main/diary-bra-main-main/instance/diary.db -> diary-bra-main-main/instance/diary-bra-main-main/instance/diary.db 	new file:   diary-bra-main-main/instance/diary-bra-main-main/main.py 	renamed:    diary-bra-main-main/diary-bra-main-main/static/css/style.css -> diary-bra-main-main/instance/diary-bra-main-main/static/css/style.css 	renamed:    diary-bra-main-main/diary-bra-main-main/static/img/login.svg -> diary-bra-main-main/instance/diary-bra-main-main/static/img/login.svg 	renamed:    diary-bra-main-main/diary-bra-main-main/static/img/logo.svg -> diary-bra-main-main/instance/diary-bra-main-main/static/img/logo.svg 	renamed:    diary-bra-main-main/diary-bra-main-main/static/img/plus.svg -> diary-bra-main-main/instance/diary-bra-main-main/static/img/plus.svg 	renamed:    diary-bra-main-main/diary-bra-main-main/static/img/post_icon.png -> diary-bra-main-main/instance/diary-bra-main-main/static/img/post_icon.png 	renamed:    diary-bra-main-main/diary-bra-main-main/templates/card.html -> diary-bra-main-main/instance/diary-bra-main-main/templates/card.html 	renamed:    diary-bra-main-main/diary-bra-main-main/templates/create_card.html -> diary-bra-main-main/instance/diary-bra-main-main/templates/create_card.html 	renamed:    diary-bra-main-main/diary-bra-main-main/templates/index.html -> diary-bra-main-main/instance/diary-bra-main-main/templates/index.html 	new file:   diary-bra-main-main/instance/diary-bra-main-main/templates/login.html 	new file:   diary-bra-main-main/instance/diary-bra-main-main/templates/registration.html 	modified:   diary-bra-main-main/instance/diary.db

--- SQL database/SQL.classes.py
+++ SQL database/SQL.classes.py
@@ -18,7 +18,7 @@ db.init_app(app)
 #criando classe
 class Usuario(db.Model):
     #o nome da tabela
-    _tablename_ = 'usuarios'
+    __tablename__ = 'usuarios'
     #identificando o registro, Integer = inteiro(para numerico)
     #String(texto), (maximo de caracteres)
     #nullable(pode estar vazio), False = nao
@@ -26,13 +26,37 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nome = db.Column(db.String(40), nullable = False, unique=True)
 
+    def __repr__ (self):
+        return f"<{self.nome}>"
+
 
 @app.route('/')
 def index():
     return "Olá, Mundo!"
 
 if __name__ == '__main__':
-    #para criar a instancia
+    #para criar a instancia ->
+    #usa so uma vez (quando inicia a pagina), depois apaga
     with app.app_context():
         db.create_all()
+
+        #para criar algo na tabela:
+        #user = Usuario(nome="Estela")
+        #db.session.add(user)
+        #db.session.commit()
+
+        #obter dados do banco de dados, query = pedido, Usuario = classe, all = tudo
+        usuarios = db.session.query(Usuario).all()
+
+        #para imprimir um registro especifico
+        #user = db.session.query(Usuario).filter_by(id = 2).first()
+        #para mudar de nome:
+        #user.nome = "Julia" - se for para deletar: db.session.delete(user)
+        #db.session.commit()
+
+        #para escrever a lista:
+        #for us in usuarios:
+            #print(f"Usuario:{us.nome}")
+
+        print(usuarios)
     app.run(debug=True)
