9045087112f3a7f5a76f2c2eed49ec15f303e09c
Mari-na20 <mportugalsilva@icloud.com>
2026-03-15 18:53:21 -0300

    modified:   CRUD - Questionario/instance/dados.db 	modified:   CRUD - Questionario/main.py 	new file:   CRUD - Questionario/templates/editar_contato.html 	modified:   CRUD - Questionario/templates/home.html 	new file:   diary-bra-main-main/instance/diary-bra-main-main/ovos-bra-main.zip 	new file:   ovos-bra-main/clouds.png 	modified:   ovos-bra-main/index.html 	modified:   ovos-bra-main/style.css

--- CRUD - Questionario/main.py
+++ CRUD - Questionario/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from db import db
 from models import Contato
 
@@ -9,7 +9,8 @@ db.init_app(app)
 #criando a funcao para chamar o html
 @app.route("/")
 def home():
-    return render_template("home.html")
+    contatos = db.session.query(Contato).all()
+    return render_template("home.html", contatos=contatos)
 
 @app.route("/registrar", methods=["GET", "POST"])
 def registrar():
@@ -21,7 +22,36 @@ def registrar():
         telefone = request.form["telefoneForm"]
         print(nome)
         print(telefone)
-        return "Usuario Registrado"
+
+        novo_contato = Contato(nome=nome,telefone=telefone )
+        db.session.add(novo_contato)
+        db.session.commit()
+        return redirect(url_for("home"))
+    
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+
+    contato = db.session.query(Contato).filter_by(id=id).first()
+
+    if request.method == "GET":
+        return render_template('editar_contato.html', contato=contato)
+
+    elif request.method == "POST":
+        nome = request.form['nomeForm']
+        telefone = request.form['telefoneForm']
+
+        contato.nome = nome
+        contato.telefone = telefone
+
+        db.session.commit()
+        return redirect(url_for('home'))
+
+@app.route("/deletar/<int:id>")
+def deletar(id):
+    contato = db.session.query(Contato).filter_by(id=id).first()
+    db.session.delete(contato)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
